00e785bebaaf5cca39cb33996525637bac9e617b
Eduardo Sánchez <eduardosr1599@gmail.com>
2022-10-16 16:39:58 -0400

    Add cpuController module and its functions

--- script.js
+++ script.js
@@ -20,7 +20,11 @@ let gameBoard=(function(){
         gameController.checkWinner();
         event.target.removeEventListener("click",markCell);
         event.target.classList.remove("available");
+        if (gameController.getCurrentPlayer().getName()==="CPU"){
+            setTimeout(cpuController.makeMove,300);
+        }
     };
+
     const initializeBoard=function(){
         for (let i=0;i<cells.length;i++){
             cells[i].setAttribute("data-index",i);
@@ -28,7 +32,6 @@ let gameBoard=(function(){
             cells[i].addEventListener("click",markCell);
         };    
     };
-    initializeBoard();
 
     let stopPlaying=function(){
         cells.forEach(cell=>{
@@ -49,6 +52,9 @@ let gameBoard=(function(){
     };
 
     let getBoard=()=>boardValues;
+    let modifyBoardValue=function(index,newValue){
+        boardValues[index]=newValue;
+    };
 
     const restartBoard=()=>{
         boardValues=[" "," "," "," "," "," "," "," "," "];
@@ -67,7 +73,11 @@ let gameBoard=(function(){
     return{
         getBoard,
         stopPlaying,
-        highlightWinnerCells
+        highlightWinnerCells,
+        modifyBoardValue,
+        renderBoardValues,
+        markCell,
+        initializeBoard
     }
 })();
 
@@ -99,6 +109,9 @@ const gameController=(function(){
         const index=Math.floor(Math.random()*2);
         currentPlayer=groupOfPlayers[index];
         resultDisplay.textContent=`${currentPlayer.getName()}'s turn`;
+        if (currentPlayer.getName()==="CPU"){
+            setTimeout(cpuController.makeMove,300);
+        }
     };
     let changeCurrentPlayer=()=>{
         if (currentPlayer===groupOfPlayers[0]){
@@ -115,7 +128,7 @@ const gameController=(function(){
         let player2Name=document.querySelector("#player2").value;
 
         if (player1Name===""){player1Name="Player 1"};
-        if (player2Name===""){player2Name="Player 2"};
+        if (player2Name==="" || player2Name==="CPU"){player2Name="Player 2"};
         let player1=Player(player1Name);
         let player2=Player(player2Name);
 
@@ -130,10 +143,37 @@ const gameController=(function(){
 
         selectFirstPlayer();
         document.querySelector(".menu").setAttribute("style","transform:scale(0)");
+        gameBoard.initializeBoard();
     }
 
     let startGameButton=document.querySelector(".start");
     startGameButton.addEventListener("click",startGamePvP);
+
+    let startGamePvCPU=function(){
+        let playerName=document.querySelector("#player").value;
+
+        if (playerName===""){playerName="Player 1"};
+        let player1=Player(playerName);
+        let player2=Player("CPU");
+
+        document.querySelector(".player2>img").setAttribute("src","./icons/robot.png");
+
+        addNewPlayer(player1);
+        addNewPlayer(player2);
+
+        let player1NameDisplay=document.querySelector(".player1>p");
+        player1NameDisplay.textContent=`${groupOfPlayers[0].getName()} ( X )`;
+
+        let player2NameDisplay=document.querySelector(".player2>p");
+        player2NameDisplay.textContent=`${groupOfPlayers[1].getName()} ( O )`;
+
+        document.querySelector(".menu").setAttribute("style","transform:scale(0)");
+        gameBoard.initializeBoard();
+        selectFirstPlayer();
+    }
+    
+    let starGameAgainstCPU=document.querySelector(".start.cpu");
+    starGameAgainstCPU.addEventListener("click",startGamePvCPU);
 
     let checkWinner=function(){
         let currentBoard=gameBoard.getBoard();
@@ -195,8 +235,8 @@ const gameController=(function(){
         
         if (currentBoard.every(cell=>cell!==" ")){
             gameBoard.stopPlaying();
-            return resultDisplay.textContent="It's a tie"
-        }
+            return resultDisplay.textContent="It's a tie";
+        };
     };
 
     return {
@@ -205,5 +245,79 @@ const gameController=(function(){
         selectFirstPlayer,
         getCurrentPlayer,
         checkWinner
+    }
+})();
+
+let cpuController=(function(){
+    let choseRandomIndex=function(){
+        let currentBoard=gameBoard.getBoard();
+        if (!(currentBoard.includes(" "))){return};
+        let randomIndex=Math.floor(Math.random()*9);
+        while (currentBoard[randomIndex]!==" "){
+            randomIndex=Math.floor(Math.random()*9);
+        }
+        return randomIndex;
+    };
+
+    let makeMove=function(){
+        let index;
+        if (typeof(checkPossibleEndGame())==="number"){
+            index=checkPossibleEndGame();
+        } else {
+            index=choseRandomIndex();
+            if (index===undefined){return}
+        };
+        gameBoard.modifyBoardValue(index,"O");
+        gameBoard.renderBoardValues();
+        document.querySelector(`div[data-index="${index}"]`).removeEventListener("click",gameBoard.markCell);
+        document.querySelector(`div[data-index="${index}"]`).classList.remove("available");
+        gameController.changeCurrentPlayer();
+        gameController.checkWinner();
+    };
+
+    let checkPossibleEndGame=function(){
+        let currentBoard=gameBoard.getBoard();
+        let reg1=/(OO )|(O O)|( OO)/;
+        let reg2=/(XX )|(X X)|( XX)/;
+
+        let regex=[reg1,reg2];
+
+        let rowsArray=[currentBoard.slice(0,3).join(""),currentBoard.slice(3,6).join(""),currentBoard.slice(6).join("")];
+        let columnsArray=[currentBoard[0]+currentBoard[3]+currentBoard[6],currentBoard[1]+currentBoard[4]+currentBoard[7],currentBoard[2]+currentBoard[5]+currentBoard[8]];
+        let diagonalsArray=[currentBoard[0]+currentBoard[4]+currentBoard[8],currentBoard[2]+currentBoard[4]+currentBoard[6]];
+
+        for (let j=0;j<2;j++){
+            for (let i=0;i<3;i++){
+                if (regex[j].test(rowsArray[i])){
+                    let match=rowsArray[i];
+                    let indexOfBlank=match.indexOf(" ");
+                    return indexOfBlank+(i*3);
+                };
+            };
+
+            for (let i=0;i<3;i++){
+                if (regex[j].test(columnsArray[i])){
+                    let match=columnsArray[i];
+                    let indexOfBlank=match.indexOf(" ");
+                    return (indexOfBlank*3)+i;
+                };
+            };
+
+            for (let i=0;i<2;i++){
+                if (regex[j].test(diagonalsArray[i])){
+                    let match=diagonalsArray[i];
+                    let indexOfBlank=match.indexOf(" ");
+                    if (i===0){
+                        return indexOfBlank*4;
+                    } else if (i===1){
+                        return (indexOfBlank+i)*2;
+                    }
+                };
+            };
+        }
+    }
+
+    return{
+        makeMove
     }
 })();
